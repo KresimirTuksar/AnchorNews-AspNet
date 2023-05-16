d7a07cadbf9b6fbc18e481649fe6e213f07b8551
Krešimir <kresimir.tuksar@gmail.com>
2023-05-16 14:40:08 +0200

    Add project files.

---
+++ AnchorNews-AspNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnchorNews.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AnchorNewsDb;Trusted_Connection=True;");
+        }
+
+        // Add DbSet properties for your models
+        // Example:
+        // public DbSet<NewsPost> NewsPosts { get; set; }
+    }
+}
